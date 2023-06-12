c31fda541a195f5dae43e7b4c073abbad1acca80
Rakesh Kumar Srivastaw <Rakesh.Srivastaw@in.niit.com>
2023-06-12 10:28:50 +0530

    Demo

---
+++ limitservice/src/main/java/com/dxc/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.dxc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dxc.config.Configuration;
+import com.dxc.model.LimitConfiguration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrive() {
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+				
+	}
+
+}
